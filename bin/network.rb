4cb47a166e822ecb5b1d71367d7fef1f66a55aae
Ryan Moore <moorer@udel.edu>
2018-09-08 14:04:52 -0400

    Add bin programs

---
+++ bin/network.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+Signal.trap("PIPE", "EXIT")
+
+iter = ARGV[0]
+btab = ARGV[1]
+
+connections = Set.new
+File.open(btab, "rt").each_line do |line|
+  source, target, *rest = line.chomp.split "\t"
+
+  connections << [source, target]
+end
+
+connections.each do |(source, target)|
+  puts [source, target, iter].join "\t"
+end
